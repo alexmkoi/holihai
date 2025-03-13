ca5cf12e5593a614cee4d1ef77ee76da1bcde6b4
alexmkoi <mrgv0986@gmail.com>
2025-03-14 01:04:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
-function playMusic() {
-    var audio = document.getElementById('holiMusic');
-    audio.play().catch(error => console.log("Auto-play blocked by browser."));
-}
-
 // Confetti Effect
 const canvas = document.getElementById('confettiCanvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let particles = [];
-for (let i = 0; i < 100; i++) {
-    particles.push({
+const confettiColors = ['#ff0', '#f00', '#0f0', '#00f', '#f0f', '#0ff', '#ff8000'];
+const confettiPieces = [];
+
+function createConfettiPiece() {
+    const piece = {
         x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height,
-        size: Math.random() * 5 + 2,
-        speed: Math.random() * 2 + 1,
-        color: `hsl(${Math.random() * 360}, 100%, 50%)`
-    });
+        y: Math.random() * canvas.height - canvas.height,
+        size: Math.random() * 10 + 5,
+        color: confettiColors[Math.floor(Math.random() * confettiColors.length)],
+        speed: Math.random() * 3 + 2,
+        rotation: Math.random() * 360,
+        rotationSpeed: Math.random() * 10 - 5
+    };
+    confettiPieces.push(piece);
 }
 
-function drawParticles() {
+function updateConfetti() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    particles.forEach(p => {
-        ctx.fillStyle = p.color;
-        ctx.beginPath();
-        ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
-        ctx.fill();
-        p.y += p.speed;
-        if (p.y > canvas.height) p.y = 0;
+    confettiPieces.forEach((piece, index) => {
+        piece.y += piece.speed;
+        piece.rotation += piece.rotationSpeed;
+        if (piece.y > canvas.height) {
+            confettiPieces.splice(index, 1);
+        }
+        drawConfettiPiece(piece);
     });
-    requestAnimationFrame(drawParticles);
+    requestAnimationFrame(updateConfetti);
 }
-drawParticles();
+
+function drawConfettiPiece(piece) {
+    ctx.save();
+    ctx.translate(piece.x, piece.y);
+    ctx.rotate((piece.rotation * Math.PI) / 180);
+    ctx.fillStyle = piece.color;
+    ctx.fillRect(-piece.size / 2, -piece.size / 2, piece.size, piece.size);
+    ctx.restore();
+}
+
+setInterval(createConfettiPiece, 100);
+updateConfetti();
